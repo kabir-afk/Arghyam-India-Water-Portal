303d2450b6ce6ec650a7e6265a14cfe20fbf7f9c
kabir-afk <kabirsingh12b@gmail.com>
2024-04-15 20:17:10 +0530

    setting the headers directly in order to bypass cors policy

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ const PORT = process.env.PORT;
 
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({
-  origin: [process.env.FRONTEND_URL],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}))
+// app.use(cors({
+//   origin: [process.env.FRONTEND_URL],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// }))
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 
 app.use("/", router);
 
